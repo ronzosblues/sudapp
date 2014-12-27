3112e25a519e9fdc2b526377a1433c685900c168
ronzosblues <ronzo.orlandini@gmail.com>
2014-12-26 23:15:19 -0300

    Volviendo

--- sudapp/config/environments/production.rb
+++ sudapp/config/environments/production.rb
@@ -20,14 +20,6 @@ Appl::Application.configure do
   :enable_starttls_auto => true
 }
 
-config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  }
-}
 
   # Settings specified here will take precedence over those in config/application.rb.
 
