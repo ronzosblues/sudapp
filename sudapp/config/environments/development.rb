bef94db306d7456b0aa9205a437c34aca3d2f203
ronzosblues <ronzo.orlandini@gmail.com>
2014-12-27 21:07:46 -0300

    Probando

--- sudapp/config/environments/development.rb
+++ sudapp/config/environments/development.rb
@@ -6,6 +6,9 @@ Appl::Application.configure do
   #config.action_mailer.raise_delivery_errors = true
   #config.action_mailer.perform_deliveries = true
   #config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
 
 
   # In the development environment your application's code is reloaded on
